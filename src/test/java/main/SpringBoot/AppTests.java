c3fd6db86234a4dcbc59cb7b0a3550791cce1c6c
VladBardin <vladyslav.bardin@gmail.com>
2023-04-24 18:50:35 +0300

    First commit

---
+++ src/test/java/main/SpringBoot/AppTests.java
@@ -0,0 +1,12 @@
+package main.SpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppTests {
+	@Test
+	void contextLoads() {
+	}
+
+}
